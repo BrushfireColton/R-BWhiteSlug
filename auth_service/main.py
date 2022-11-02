95b9386fa455609b2b2be76748ac0cfca5024ee1
jshep23 <justin.shepertycki@gmail.com>
2022-11-02 14:28:18 -0500

    remove unused endpoint in auth service

--- auth_service/main.py
+++ auth_service/main.py
@@ -1,8 +1,6 @@
-from fastapi import FastAPI, Depends, Request, status
+from fastapi import FastAPI, status
 from dotenv import load_dotenv
-import os
-from auth_service import AuthService
-from di_container import create_auth_service
+
 
 load_dotenv()
 app = FastAPI()
@@ -11,14 +9,3 @@ app = FastAPI()
 @app.get("/api/v1/healthz", status_code=status.HTTP_200_OK)
 def health_check() -> dict:
     return {"status": "OK"}
-
-
-@app.post("/get_token")
-async def get_token(
-    request: Request, auth_service: AuthService = Depends(create_auth_service)
-):
-    request_body = await request.json()
-    redirect_response = request_body["redirect_response"]
-    return auth_service.get_token(
-        os.getenv("TOKEN_URL"), redirect_response=redirect_response
-    )
