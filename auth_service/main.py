ad1c2d879b631501c3b932104fd3a649665d59dd
jshep23 <justin.shepertycki@gmail.com>
2022-11-02 15:29:34 -0500

    added validate endpoint

--- auth_service/main.py
+++ auth_service/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Header, Response
 from dotenv import load_dotenv
 
 
@@ -9,3 +9,16 @@ app = FastAPI()
 @app.get("/api/v1/healthz", status_code=status.HTTP_200_OK)
 def health_check() -> dict:
     return {"status": "OK"}
+
+
+@app.get("/api/v1/validate")
+def validate_request(
+    response: Response, authorization: str | None = Header(default=None)
+):
+    result: dict = {"authorized": True}
+
+    if authorization is None:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        result["authorized"] = False
+
+    return result
