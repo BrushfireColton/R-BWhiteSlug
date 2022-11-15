0093870edba9f3ba8e1e983becc78378f48347b7
jshep23 <justin.shepertycki@gmail.com>
2022-11-14 21:08:32 -0600

    fix newline lint error

--- vendor_service/repositories/profile_repository.py
+++ vendor_service/repositories/profile_repository.py
@@ -3,7 +3,7 @@ import abc
 from domain.membership import Membership
 
 
-class ProfileRepository (metaclass=abc.ABCMeta):
+class ProfileRepository(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def get_profile(self, api_key: str, token: str, membership: Membership):
         raise NotImplementedError
