726d25b2c30a950ff7fc86d3caddbb80a345700d
ColtonSimard <csimard@fasadmin.com>
2022-11-23 14:53:37 -0700

    fake bungie data

--- vendor_service/repositories/membership_repository_impl.py
+++ vendor_service/repositories/membership_repository_impl.py
@@ -8,25 +8,29 @@ from repositories.membership_repository import MembershipRepository
 class MembershipRepositoryImpl(MembershipRepository):
     def get_memberships(self, api_key: str, token: str) -> List[Membership]:
 
-        headers = {"X-API-Key": api_key, "Authorization": token}
-        result = requests.get(
-            "https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/",
-            headers=headers,
-        )
+        # headers = {"X-API-Key": api_key, "Authorization": token}
+        # result = requests.get(
+        #     "https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/",
+        #     headers=headers,
+        # )
 
-        json_data = result.json()["Response"]
+        # json_data = result.json()["Response"]
 
-        primary_membership_id = json_data["primaryMembershipId"] if "primaryMembershipId" in json_data else None
+        # primary_membership_id = json_data["primaryMembershipId"] if "primaryMembershipId" in json_data else None
         
 
-        return list(
-            map(
-                lambda membership: Membership(
-                    id=membership["membershipId"],
-                    primary_membership_id=primary_membership_id,
-                    display_name=membership["displayName"],
-                    membership_type=membership["membershipType"],
-                ),
-                json_data["destinyMemberships"],
-            )
-        )
+        # return list(
+        #     map(
+        #         lambda membership: Membership(
+        #             id=membership["membershipId"],
+        #             primary_membership_id=primary_membership_id,
+        #             display_name=membership["displayName"],
+        #             membership_type=membership["membershipType"],
+        #         ),
+        #         json_data["destinyMemberships"],
+        #     )
+        # )
+
+        return [
+            Membership('best', '466546', 'King', 852)
+        ]
--- vendor_service/repositories/profile_repository_impl.py
+++ vendor_service/repositories/profile_repository_impl.py
@@ -13,17 +13,20 @@ from repositories.profile_repository import ProfileRepository
 
 class ProfileRepositoryImpl(ProfileRepository):
     def get_profile(self, api_key: str, token: str, membership: Membership) -> Profile:
-        headers = {"X-API-Key": api_key, "Authorization": token}
+        print(api_key)
+        print(token)
+        print(membership)
+        # headers = {"X-API-Key": api_key, "Authorization": token}
 
-        result = requests.get(f"https://bungie.net/Platform/Destiny2/{membership.membership_type}/Profile/{membership.id}/?components=200", headers=headers)
+        # result = requests.get(f"https://bungie.net/Platform/Destiny2/{membership.membership_type}/Profile/{membership.id}/?components=200", headers=headers)
 
-        json_response = result.json()['Response']
-        characters_dict: dict = json_response['characters']['data']
+        # json_response = result.json()['Response']
+        # characters_dict: dict = json_response['characters']['data']
         characters: List[Character] = []
 
-        character_keys = characters_dict.keys()
-        for key in character_keys:
-            character_data = characters_dict[key]
-            characters.append(Character(key, ClassType(int(character_data['classType']))))
+        # character_keys = characters_dict.keys()
+        for number in range(3):
+            # character_data = characters_dict[key]
+            characters.append(Character(str(number), class_type=ClassType.Titan))
 
         return Profile(characters=characters)
